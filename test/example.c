118348b7c8f00e0cc81c3b4a402f422244e997e7
dkrotx <jdkrot@gmail.com>
2013-02-01 00:40:34 +0400

    Add test file and INSTALL instr

---
+++ test/example.c
@@ -0,0 +1,42 @@
+/* demo file for crxprof
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* This function should be seen only with -r (realtime) option
+ */
+void sleeping_fn(int us) {
+    usleep(us);
+}
+
+#define CALC_NOTHING(how_much) do {            \
+    int i, j, sum = 0;                         \
+    for (i = 0; i < (how_much) * 10000; i++)   \
+        for (j = 1; j < 1000; j++)             \
+            sum += ( i * j ) | ( i / j );      \
+    } while(0)
+
+void fn() {
+    CALC_NOTHING(1);
+    sleeping_fn(1500);
+}
+
+void heavy_fn() {
+    CALC_NOTHING(2);
+    fn();
+}
+
+
+int main()
+{
+    printf("PID: %d\n", (int)getpid());
+
+    for(;;) {
+        heavy_fn();
+        fn();
+        printf("One more cycle\n");
+    }
+
+    return 0;
+}
